7c0a612159f92f00b0ea37bf4d9e69fcab6a8bdb
Reetro <thegamerboy7@gmail.com>
2020-06-09 17:30:05 -0600

    started work on basic player movement

---
+++ Assets/Scripts/General Scripts/GeneralFunctions.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneralFunctions
+{
+    // if the given object has a Rigidbody2D it will check to see which way the object is moving and flip it's sprite in the given direction
+    public static void FlipSprite(GameObject objectToFlip, float horzontial)
+    {
+        if (horzontial > 0)
+        {
+            objectToFlip.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+        else
+        {
+            objectToFlip.transform.localRotation = Quaternion.Euler(0f, 180f, 0f);
+        }
+    }
+
+    // will check to see if the given object has a Rigidbody2D and if it does will see if it's velocity is greater than zero
+    public static bool IsObjectMovingHorizontaly(GameObject objectToTest)
+    {
+        var rigidBody = objectToTest.GetComponent<Rigidbody2D>();
+
+        if (rigidBody)
+        {
+            return Mathf.Abs(rigidBody.velocity.x) > Mathf.Epsilon;
+        }
+        else
+        {
+            Debug.LogError(objectToTest.ToString() + "didn't have a a Rigidbody2D was unable to test it horizontally");
+            return false;
+        }
+    }
+}
---
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Running Settings")]
+    [SerializeField] float runSpeed = 2f;
+
+    [Header("Jump Settings")]
+    [SerializeField] float jumpForce = 10f;
+
+    Rigidbody2D _rigidbody = null;
+    BoxCollider2D _myCollision = null;
+
+    float horizontalAxis = 0f;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _myCollision = GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        MovePlayer();
+        Jump();
+
+        if (GeneralFunctions.IsObjectMovingHorizontaly(gameObject))
+        {
+            GeneralFunctions.FlipSprite(gameObject, horizontalAxis);
+        }
+    }
+
+    private void MovePlayer()
+    {
+        horizontalAxis = Input.GetAxis("Horizontal");
+
+        transform.position += new Vector3(horizontalAxis, 0f, 0f) * Time.deltaTime * runSpeed;
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && _myCollision.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            _rigidbody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+}
