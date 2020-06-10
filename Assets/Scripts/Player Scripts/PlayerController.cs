404eac29405d0a7b2935629da0c10c8414c1e63d
Reetro <thegamerboy7@gmail.com>
2020-06-10 16:17:22 -0600

    setup player gun prefab it will rotate towards the mouse

--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -26,16 +26,32 @@ public class PlayerController : MonoBehaviour
             jump = true;
             myAnimator.SetBool("IsJumping", true);
         }
+
+        LookatMouse();
     }
 
     private void FixedUpdate()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, jump, true);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, jump, false);
         jump = false;
     }
 
     public void OnLanding()
     {
         myAnimator.SetBool("IsJumping", false);
+    }
+
+    private void LookatMouse()
+    {
+        Vector3 mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (mousepos.x < transform.position.x)
+        {
+            transform.eulerAngles = new Vector3(transform.position.x, 180f, transform.position.z);
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(transform.position.x, 0f, transform.position.z);
+        }
     }
 }
