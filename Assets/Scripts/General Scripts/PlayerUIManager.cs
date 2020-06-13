7fe449d7de88dce95a85a001626698d55996e15c
Reetro <thegamerboy7@gmail.com>
2020-06-13 10:52:31 -0600

    buff icons now spawn at the correct size

--- Assets/Scripts/General Scripts/PlayerUIManager.cs
+++ Assets/Scripts/General Scripts/PlayerUIManager.cs
@@ -7,14 +7,14 @@ using UnityEngine.UI;
 public class PlayerUIManager : MonoBehaviour
 {
     [SerializeField] GridLayoutGroup buffGridLayoutGroup = null;
+    [SerializeField] GridLayoutGroup debuffGridLayoutGroup = null;
     [SerializeField] BuffIcon buffIconPrefab = null;
 
     private List<BuffIcon> buffIcons = new List<BuffIcon>();
 
     public BuffIcon AddBuffIcon(ScriptableBuff buff)
     {
-        BuffIcon icon = Instantiate(buffIconPrefab, transform.position, Quaternion.identity);
-        icon.transform.SetParent(buffGridLayoutGroup.transform);
+        BuffIcon icon = Instantiate(buffIconPrefab, buffGridLayoutGroup.transform);
         buffIcons.Add(icon);
 
         icon.StartCooldown(buff);
