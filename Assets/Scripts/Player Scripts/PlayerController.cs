72daa1345353aced00d590929d71b40820b73f1c
Reetro <thegamerboy7@gmail.com>
2020-06-19 17:24:15 -0600

    Reworked input system to detect gamepads

--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerController : MonoBehaviour
 {
@@ -6,7 +7,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] GameObject leechCollision = null;
     [SerializeField] GameObject playerState = null;
     [SerializeField] PlayerUIManager uiManager = null;
-
+    
     [Header("Run Settings")]
     [SerializeField] float runSpeed = 40f;
 
@@ -30,7 +31,7 @@ public class PlayerController : MonoBehaviour
     {
         if (!myHealthComp.GetIsDead())
         {
-            horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+            horizontalMove = CrossPlatformInputManager.GetAxisRaw("Horizontal") * runSpeed;
 
             myAnimator.SetFloat("Speed", horizontalMove);
 
@@ -43,13 +44,18 @@ public class PlayerController : MonoBehaviour
                 myAnimator.SetBool("Idle", false);
             }
 
-            if (Input.GetButtonDown("Jump"))
+            if (CrossPlatformInputManager.GetButtonDown("Jump"))
             {
                 jump = true;
                 myAnimator.SetBool("IsJumping", true);
             }
 
-            if (Input.GetButtonDown("Fire1"))
+            if (CrossPlatformInputManager.GetButtonDown("Fire1"))
+            {
+                currentGun.FireGun();
+            }
+
+            if (CrossPlatformInputManager.GetAxis("TriggerFire") > 0.1)
             {
                 currentGun.FireGun();
             }
--- Assets/Scripts/Player Scripts/PlayerGun.cs
+++ Assets/Scripts/Player Scripts/PlayerGun.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerGun : MonoBehaviour
 {
@@ -15,11 +16,12 @@ public class PlayerGun : MonoBehaviour
     [SerializeField] private HealthComponent playerHealthComp = null;
 
     private float gunAngle = 0f;
+    private bool gamePadActive = false;
 
     void Update()
     {
-        RotateGunToMouse();
-        RotatePlayer();
+        RotateGun();
+        RotatePlayerToMouse();
     }
 
     public void FireGun()
@@ -34,39 +36,77 @@ public class PlayerGun : MonoBehaviour
         }
     }
 
-    private void RotatePlayer()
+    private void RotatePlayerToMouse()
     {
         if (!playerHealthComp.GetIsDead())
         {
-            if (MouseLeftOrRight())
+            if (!gamePadActive)
             {
-                controller.transform.eulerAngles = new Vector3(transform.position.x, 180f, transform.position.z);
-            }
-            else
-            {
-                controller.transform.eulerAngles = new Vector3(transform.position.x, 0f, transform.position.z);
+                if (MouseLeftOrRight())
+                {
+                    controller.transform.eulerAngles = new Vector3(transform.position.x, 180f, transform.position.z);
+                }
+                else
+                {
+                    controller.transform.eulerAngles = new Vector3(transform.position.x, 0f, transform.position.z);
+                }
             }
         }
     }
 
-    private void RotateGunToMouse()
+    private void RotateGun()
     {
         if (!playerHealthComp.GetIsDead())
         {
-            Vector3 mousePos = Input.mousePosition;
-            Vector3 gunPos = Camera.main.WorldToScreenPoint(transform.position);
-
-            mousePos.x = mousePos.x - gunPos.x;
-            mousePos.y = mousePos.y - gunPos.y;
-            gunAngle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
-
-            if (MouseLeftOrRight())
+            if (!gamePadActive)
             {
-                transform.rotation = Quaternion.Euler(new Vector3(180f, 0f, -gunAngle));
+                Vector3 mousePos = CrossPlatformInputManager.mousePosition;
+                Vector3 gunPos = Camera.main.WorldToScreenPoint(transform.position);
+
+                mousePos.x = mousePos.x - gunPos.x;
+                mousePos.y = mousePos.y - gunPos.y;
+                gunAngle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+                if (MouseLeftOrRight())
+                {
+                    transform.rotation = Quaternion.Euler(new Vector3(180f, 0f, -gunAngle));
+                }
+                else
+                {
+                    transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, gunAngle));
+                }
             }
             else
             {
-                transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, gunAngle));
+                float deadzone = 0.25f;
+                Vector2 stickInput = new Vector2(CrossPlatformInputManager.GetAxis("HorizontalRightStick"), CrossPlatformInputManager.GetAxis("VerticalRightStick"));
+
+                if (stickInput.magnitude < deadzone)
+                {
+                    stickInput = Vector2.zero;
+                }
+                else
+                {
+                    stickInput = stickInput.normalized * ((stickInput.magnitude - deadzone) / (1 - deadzone));
+                }
+
+                if (stickInput.sqrMagnitude >= 0.1)
+                {
+                    gunAngle = Mathf.Atan2(stickInput.x, stickInput.y) * Mathf.Rad2Deg;
+
+                    if (stickInput.y < 0)
+                    {
+                        transform.rotation = Quaternion.Euler(new Vector3(180f, 0f, -gunAngle));
+
+                        controller.transform.eulerAngles = new Vector3(transform.position.x, 180f, transform.position.z);
+                    }
+                    else
+                    {
+                        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, gunAngle));
+
+                        controller.transform.eulerAngles = new Vector3(transform.position.x, 0f, transform.position.z);
+                    }
+                }
             }
         }
     }
@@ -74,8 +114,28 @@ public class PlayerGun : MonoBehaviour
     private bool MouseLeftOrRight()
     {
         var playerScreenPoint = Camera.main.WorldToScreenPoint(controller.transform.position);
-        float mouseX = Input.mousePosition.x;
+        float mouseX = CrossPlatformInputManager.mousePosition.x;
 
         return mouseX < playerScreenPoint.x ? true : false;
+    }
+
+    public void UpdateInput(bool gamePadActive)
+    {
+        if (gamePadActive)
+        {
+            this.gamePadActive = true;
+
+            Cursor.visible = false;
+
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else
+        {
+            this.gamePadActive = false;
+
+            Cursor.visible = true;
+
+            Cursor.lockState = CursorLockMode.None;
+        }
     }
 }
