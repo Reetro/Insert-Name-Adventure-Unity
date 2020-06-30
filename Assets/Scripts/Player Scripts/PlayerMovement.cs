a95cda33a51e962f547efd0ed0ee29962edd401d
Reetro <thegamerboy7@gmail.com>
2020-06-30 11:50:01 -0600

    Player will no longer slide off moving platforms

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -91,7 +91,7 @@ public class PlayerMovement : MonoBehaviour
 		}
 	}
 	// Attach player to a moving platform
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
 		if (collision.gameObject.CompareTag("Platform"))
         {
