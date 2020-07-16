e11d3b2c31c65e61297e586b6e06bcaadda6c060
Reetro <wells.justin70@yahoo.com>
2020-07-16 17:02:48 -0600

    move leech collision into it's own namespace

--- Assets/Scripts/Player Scripts/Leech Collision/LeechAttachCollisionLeft.cs
+++ Assets/Scripts/Player Scripts/Leech Collision/LeechAttachCollisionLeft.cs
@@ -1,43 +1,46 @@
 ﻿using UnityEngine;
 using AuraSystem;
 
-public class LeechAttachCollisionLeft : MonoBehaviour
+namespace PlayerCharacter.Collision
 {
-    [SerializeField] private AttachedLeech leechToAttach = null;
-    [SerializeField] private Vector2 offset = new Vector2(0, 0);
-    [SerializeField] private string tagToAttach = "Leech Collision Top";
-
-    private GameObject player = null;
-    private AuraManager auraManager = null;
-
-    private void Awake()
+    public class LeechAttachCollisionLeft : MonoBehaviour
     {
-        player = GameObject.FindGameObjectWithTag("Player");
+        [SerializeField] private AttachedLeech leechToAttach = null;
+        [SerializeField] private Vector2 offset = new Vector2(0, 0);
+        [SerializeField] private string tagToAttach = "Leech Collision Top";
 
-        auraManager = player.GetComponent<AuraManager>();
-    }
+        private GameObject player = null;
+        private AuraManager auraManager = null;
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Attachable Leech"))
+        private void Awake()
         {
-            if (GeneralFunctions.CanLeechAttach(tagToAttach) && !GeneralFunctions.IsObjectDead(collision.gameObject))
+            player = GameObject.FindGameObjectWithTag("Player");
+
+            auraManager = player.GetComponent<AuraManager>();
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("Attachable Leech"))
             {
-                var leechHP = collision.gameObject.GetComponent<HealthComponent>();
+                if (GeneralFunctions.CanLeechAttach(tagToAttach) && !GeneralFunctions.IsObjectDead(collision.gameObject))
+                {
+                    var leechHP = collision.gameObject.GetComponent<HealthComponent>();
 
-                var spawnTransform = GeneralFunctions.GetLeechAttachPointByTag(tagToAttach);
+                    var spawnTransform = GeneralFunctions.GetLeechAttachPointByTag(tagToAttach);
 
-                var spawnLeech = GeneralFunctions.SpawnLeechAttach(auraManager, leechToAttach, spawnTransform, leechHP.CurrentHealth, player);
+                    var spawnLeech = GeneralFunctions.SpawnLeechAttach(auraManager, leechToAttach, spawnTransform, leechHP.CurrentHealth, player);
 
-                GeneralFunctions.FlipObject(spawnLeech.gameObject);
+                    GeneralFunctions.FlipObject(spawnLeech.gameObject);
 
-                Destroy(collision.gameObject);
+                    Destroy(collision.gameObject);
+                }
             }
         }
-    }
 
-    private void LateUpdate()
-    {
-        transform.position = player.transform.position + (Vector3)offset;
+        private void LateUpdate()
+        {
+            transform.position = player.transform.position + (Vector3)offset;
+        }
     }
 }
