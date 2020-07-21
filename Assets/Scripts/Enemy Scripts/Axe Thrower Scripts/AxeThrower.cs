581ef00d0ffe40eded545d0477b16400f27607c8
Reetro <wells.justin70@yahoo.com>
2020-07-21 17:53:48 -0600

    Started work on the axe thrower

---
+++ Assets/Scripts/Enemy Scripts/Axe Thrower Scripts/AxeThrower.cs
@@ -0,0 +1,17 @@
+﻿using LevelObjects.MovingObjects;
+using System;
+using UnityEngine;
+
+namespace EnemyCharacter.AI
+{
+    [Serializable]
+    public class AxeThrower : EnemyShooter
+    {
+        protected override void Shoot()
+        {
+            ProjectileMovement axe = Instantiate(ProjectileToShoot, FireTransform.position, Quaternion.identity);
+
+
+        }
+    }
+}
