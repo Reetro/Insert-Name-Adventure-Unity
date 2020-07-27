3b9f6382a4ee393127ecd7fc78e5a3354b7bc81c
Reetro <wells.justin70@yahoo.com>
2020-07-27 17:09:20 -0600

    fixes leech AI bugs

--- Assets/Scripts/Gameplay Scripts/Movement Scripts/LeechMovement.cs
+++ Assets/Scripts/Gameplay Scripts/Movement Scripts/LeechMovement.cs
@@ -14,26 +14,15 @@ namespace EnemyCharacter.AI
         [SerializeField] private float randomYMin = 0.005f;
         [Tooltip("Maximum amount to add to the leeches current Y vector")]
         [SerializeField] private float randomYMax = 0.007f;
-        [Tooltip("Multiplier used to knock leech downward")]
-        [SerializeField] private float forceMultipler = 0.8f;
-        [Tooltip("Delay for downward force")]
-        [SerializeField] private float pushTimer = 2f;
-        [Tooltip("What layers can add force to leech")]
-        public LayerMask ForceLayer;
-
-        private void OnCollisionStay2D(Collision2D collision)
-        {
-            if (CheckCollision())
-            {
-                StartCoroutine(ApplyDownForce());
-            }
-        }
 
         private void Update()
         {
             if (!MyHealthComponent.IsCurrentlyDead)
             {
-                RotateToMovement();
+                if (MyRigidBody2D.velocity.magnitude > 0)
+                {
+                    RotateToMovement();
+                }
 
                 var amountToAddToY = GeneralFunctions.CreateRandomVector2OnlyY(randomYMin, randomYMax);
 
@@ -61,30 +50,6 @@ namespace EnemyCharacter.AI
         public void OnDeathAnimationEnd()
         {
             Destroy(gameObject);
-        }
-
-        private IEnumerator ApplyDownForce()
-        {
-            yield return new WaitForSeconds(pushTimer);
-
-            if (!MyHealthComponent.IsCurrentlyDead)
-            {
-                MyRigidBody2D.AddForce(-transform.up * forceMultipler, ForceMode2D.Impulse);
-            }
-        }
-
-        private bool CheckCollision()
-        {
-            RaycastHit2D raycastHit2D = Physics2D.Raycast(transform.position, transform.up, 1f, ForceLayer);
-            
-            if (raycastHit2D)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
         }
     }
 }
