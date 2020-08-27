d18d9ccb4d3c097e31cbea3bba379460f615f2a7
Reetro <wells.justin70@yahoo.com>
2020-08-27 15:53:39 -0600

    Fixed worm segment collision check function

--- Assets/Scripts/Enemy Scripts/Worm Scripts/WormSegment.cs
+++ Assets/Scripts/Enemy Scripts/Worm Scripts/WormSegment.cs
@@ -7,8 +7,6 @@ namespace EnemyCharacter.AI
     {
         private GameplayObjectID idObject = null;
         private SpriteRenderer spriteRenderer = null;
-        private Vector2 boxSize;
-        private float boxAngle = 0;
 
         [System.Serializable]
         public class OnSegmentDeath : UnityEvent<WormSegment> { }
@@ -16,6 +14,7 @@ namespace EnemyCharacter.AI
         [HideInInspector]
         public OnSegmentDeath SegmentDeath;
 
+        #region Collision Functions
         /// <summary>
         /// Set all needed references
         /// </summary>
@@ -25,12 +24,9 @@ namespace EnemyCharacter.AI
             MyHealthComponent = GetComponent<HealthComponent>();
             spriteRenderer = GetComponent<SpriteRenderer>();
             MyBoxCollider2D = GetComponent<BoxCollider2D>();
+            MyCapsuleCollider2D = GetComponent<CapsuleCollider2D>();
 
             MyWidth = GeneralFunctions.GetSpriteWidth(GetComponent<SpriteRenderer>());
-
-            boxSize = MyBoxCollider2D.size;
-
-            boxAngle = GeneralFunctions.GetObjectAngle(gameObject);
 
             idObject.ConstructID();
             MyHealthComponent.ConstructHealthComponent();
@@ -51,95 +47,17 @@ namespace EnemyCharacter.AI
         /// </summary>
         public void CheckCollision()
         {
-            var traceDown = (Vector2)transform.position - Vector2.down * MyWidth;
-
-            if (CheckCast(traceDown))
-            {
-                UnFreezeSegment();
-                AboveGround = true;
-            }
-            else
-            {
-                CheckOverlapBox(traceDown);
-            }
-        }
-        /// <summary>
-        /// Fires a trace downward to check for ground
-        /// </summary>
-        /// <param name="traceDown"></param>
-        /// <returns>A bool</returns>
-        private bool CheckCast(Vector2 traceDown)
-        {
-            RaycastHit2D raycastHit2DDown = Physics2D.Raycast(transform.position, traceDown, 2f, WhatIsGround);
-
-            if (raycastHit2DDown)
-            {
-                if (DrawDebug)
-                {
-                    Debug.DrawLine(transform.position, traceDown * 2f, Color.green);
-                }
-
-                return true;
-            }
-            else
-            {
-                if (DrawDebug)
-                {
-                    Debug.DrawLine(transform.position, traceDown * 2f, Color.red);
-                }
-
-                return false;
-            }
-        }
-        /// <summary>
-        /// Draws a box and checks to see if anything is overlapping it if not enable collision
-        /// </summary>
-        /// <param name="traceDown"></param>
-        private void CheckOverlapBox(Vector2 traceDown)
-        {
-            Collider2D collider2D = Physics2D.OverlapBox(transform.position, boxSize, boxAngle, WhatIsGround);
+            Collider2D collider2D = Physics2D.OverlapCapsule(transform.position, MyCapsuleCollider2D.size, MyCapsuleCollider2D.direction, GeneralFunctions.GetObjectAngle(gameObject), WhatIsGround);
 
             if (collider2D)
             {
-                FreezeSegment();
                 AboveGround = false;
+
+                print("Hit: " + collider2D.gameObject.name);
             }
             else
             {
-                var traceUp = (Vector2)transform.position + Vector2.up / MyWidth;
-
-                RaycastHit2D raycastHit2DUp = Physics2D.Raycast(transform.position, traceUp, 1f, WhatIsGround);
-
-                if (raycastHit2DUp)
-                {
-                    if (DrawDebug)
-                    {
-                        Debug.DrawLine(transform.position, traceUp * 1f, Color.green);
-                    }
-
-                    FreezeSegment();
-                    AboveGround = false;
-                }
-                else
-                {
-                    if (DrawDebug)
-                    {
-                        Debug.DrawLine(transform.position, traceUp * 1f, Color.red);
-                    }
-
-                    UnFreezeSegment();
-                    AboveGround = true;
-                }
-            }
-        }
-        /// <summary>
-        /// If debug is active draw collision cube
-        /// </summary>
-        private void OnDrawGizmos()
-        {
-            if (DrawDebug)
-            {
-                Gizmos.DrawCube(transform.position, boxSize);
+                AboveGround = true;
             }
         }
         /// <summary>
@@ -156,20 +74,9 @@ namespace EnemyCharacter.AI
                 }
             }
         }
-        /// <summary>
-        /// Freeze this segment in it's current position
-        /// </summary>
-        public void FreezeSegment()
-        {
-            MyBoxCollider2D.enabled = false;
-        }
-        /// <summary>
-        /// Allow segment to moved again
-        /// </summary>
-        public void UnFreezeSegment()
-        {
-            MyBoxCollider2D.enabled = true;
-        }
+        #endregion
+
+        #region Properties
         /// <summary>
         /// Amount of damage to apply to the player
         /// </summary>
@@ -187,6 +94,10 @@ namespace EnemyCharacter.AI
         /// </summary>
         public BoxCollider2D MyBoxCollider2D { get; private set; } = null;
         /// <summary>
+        /// CapsuleCollider2D attached to segment
+        /// </summary>
+        public CapsuleCollider2D MyCapsuleCollider2D { get; private set; } = null;
+        /// <summary>
         /// What layers are ground
         /// </summary>
         public LayerMask WhatIsGround { get; set; }
@@ -202,5 +113,6 @@ namespace EnemyCharacter.AI
         /// Get the width of the worm segment sprite
         /// </summary>
         public float MyWidth { get; private set; }
+        #endregion
     }
 }
