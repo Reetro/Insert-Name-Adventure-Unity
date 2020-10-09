4ffcfe14094cddb8fbde656f6f709de08eb5ac02
Reetro <wells.justin70@yahoo.com>
2020-10-09 09:12:47 -0600

    Attached Leech no longer get damaged by trap projectiles

--- Assets/Scripts/Enemy Scripts/Leech Scripts/AttachedLeech.cs
+++ Assets/Scripts/Enemy Scripts/Leech Scripts/AttachedLeech.cs
@@ -16,6 +16,12 @@ namespace EnemyCharacter
         /// </summary>
         public int MyID { get; private set; }
 
+        /// <summary>
+        /// Called after a leech has been spawned
+        /// </summary>
+        /// <param name="health"></param>
+        /// <param name="player"></param>
+        /// <param name="id"></param>
         public void OnLeechSpawn(float health, GameObject player, int id)
         {
             MyID = id;
@@ -27,8 +33,13 @@ namespace EnemyCharacter
             leechHealthComp.OnDeath.AddListener(OnDeath);
 
             debuffEffect = GeneralFunctions.ApplyDebuffToTarget(player, leechingDebuff, true);
-        }
 
+            // Ignore trap projectile collision
+            Physics2D.IgnoreLayerCollision(14, 20, true);
+        }
+        /// <summary>
+        /// Called when an attached leech has been killed
+        /// </summary>
         public void OnDeath()
         {
             debuffEffect.RemoveFromStack(true, debuffEffect);
