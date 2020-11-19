ab2feceaa5513844d77648f44cfebec4b698681d
Reetro <wells.justin70@yahoo.com>
2020-11-18 17:39:06 -0700

    Cleaned Up Worm Movement Code

--- Assets/Scripts/Enemy Scripts/Worm Scripts/WormMovement.cs
+++ Assets/Scripts/Enemy Scripts/Worm Scripts/WormMovement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace EnemyCharacter.AI
@@ -9,12 +10,50 @@ namespace EnemyCharacter.AI
         /// How much health each segment has
         /// </summary>
         [SerializeField] private float segmentHealth = 6;
+        /// <summary>
+        /// Set's which layers the worm considers ground
+        /// </summary>
+        [SerializeField] private LayerMask whatIsGround = new LayerMask();
+
+        [SerializeField] private float launchDelay = 1f;
+
+        [SerializeField] private float launchSpeed = 2f;
+
+        [SerializeField] private float endPointMultiplier = 10f;
+
+        [SerializeField] private float inAirTimer = 2f;
+
+        [SerializeField] private float sinkDelay = 2f;
+
+        private Vector2 playerPostion = new Vector2();
+        private Vector3 launchEndPoint = new Vector3();
+        private float deafaultInAirTimer = 0f;
+        private float deafaultSinkDelay = 0f;
+        private bool runMoveTimer = false;
+        private bool runSinkTimer = false;
+        private bool gotSinkTarget = false;
+        private Vector2 sinkTarget = Vector2.zero;
+
+        /// <summary>
+        /// All worm movement types
+        /// </summary>
+        public enum WormMovementState
+        {
+            FollowPlayer,
+            IsLaunching,
+            RotateInAir,
+            MoveToGround,
+            SinkToGround
+        }
 
         /// <summary>
         /// Called right after the SceneCreator has setup the Player Gameobject
         /// </summary>
         public void OnSceneCreated()
         {
+            deafaultInAirTimer = inAirTimer;
+            deafaultSinkDelay = sinkDelay;
+
             AllChildSegments = GetComponentsInChildren<WormSegment>();
 
             for (int index = 0; index < AllChildSegments.Length; index++)
@@ -23,12 +62,206 @@ namespace EnemyCharacter.AI
 
                 if (segment)
                 {
-                    segment.SetupWormSegment(index, segmentHealth);
+                    segment.SetupWormSegment(index, segmentHealth, whatIsGround);
 
                     segment.SegmentDeath.AddListener(OnSegmentDeath);
                 }
             }
+
+            if (!AreAllWormSegmentsUp())
+            {
+                CurrentMovementState = WormMovementState.FollowPlayer;
+            }
+
+            StartCoroutine(StartLaunch());
         }
+
+        #region Movement Code
+        /// <summary>
+        /// Check to see if worm segments are above ground if so enable collision         
+        /// </summary>
+        private void Update()
+        {
+            foreach (WormSegment wormSegment in AllChildSegments)
+            {
+                if (wormSegment)
+                {
+                    if (!wormSegment.MyHealthComponent.IsCurrentlyDead)
+                    {
+                        if (wormSegment.IsOverlappingGround())
+                        {
+                            wormSegment.DisableCollision();
+                        }
+                        else
+                        {
+                            wormSegment.EnableCollision();
+                        }
+                    }
+                }
+            }
+
+            switch (CurrentMovementState)
+            {
+                case WormMovementState.FollowPlayer:
+                    FollowPlayerX();
+                    break;
+                case WormMovementState.IsLaunching:
+                    LaunchWorm();
+                    break;
+                case WormMovementState.RotateInAir:
+                    RotateWorm();
+                    break;
+                case WormMovementState.MoveToGround:
+                    MoveWormToGround();
+                    break;
+                case WormMovementState.SinkToGround:
+                    SinkToGround();
+                    break;
+            }
+        }
+        /// <summary>
+        /// Follow player along the X axis
+        /// </summary>
+        private void FollowPlayerX()
+        {
+            playerPostion = GeneralFunctions.GetPlayerGameObject().transform.position;
+
+            transform.position = new Vector3(playerPostion.x, transform.position.y, transform.position.z);
+        }
+        /// <summary>
+        /// Launch the worm up towards the launch end point
+        /// </summary>
+        private void LaunchWorm()
+        {
+            float step = launchSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, launchEndPoint, step);
+
+            if (Vector3.Distance(transform.position, launchEndPoint) < 0.001f)
+            {
+                CurrentMovementState = WormMovementState.RotateInAir;
+            }
+        }
+        /// <summary>
+        /// Rotate Worm down to -90 degrees
+        /// </summary>
+        private void RotateWorm()
+        {
+            var segmentToRotate = AllChildSegments[0];
+            var targetRotation = new Vector3(0, 0, -90);
+
+            segmentToRotate.transform.Rotate(targetRotation);
+
+            CurrentMovementState = WormMovementState.MoveToGround;
+            runMoveTimer = true;
+        }
+        /// <summary>
+        /// Move the worm back to the ground
+        /// </summary>
+        private void MoveWormToGround()
+        {
+            if (runMoveTimer)
+            {
+                inAirTimer -= Time.deltaTime;
+            }
+
+            if (inAirTimer <= 0)
+            {
+                runMoveTimer = false;
+
+                var targetLocation = GetGroundPoint();
+
+                float step = launchSpeed * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, targetLocation, step);
+
+                if (Vector3.Distance(transform.position, targetLocation) < 0.001f)
+                {
+                    CurrentMovementState = WormMovementState.SinkToGround;
+
+                    inAirTimer = deafaultInAirTimer;
+
+                    runSinkTimer = true;
+                }
+            }
+        }
+        /// <summary>
+        /// Sink the worm back into the ground and reset rotation back to 0
+        /// </summary>
+        private void SinkToGround()
+        {
+            if (runSinkTimer)
+            {
+                sinkDelay -= Time.deltaTime;
+            }
+            
+            if (sinkDelay <= 0)
+            {
+                if (!gotSinkTarget)
+                {
+                    gotSinkTarget = true;
+
+                    sinkTarget = (Vector2)transform.position + new Vector2(0, -5.5f);
+                }
+
+                float step = launchSpeed * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, sinkTarget, step);
+
+                if (Vector3.Distance(transform.position, sinkTarget) < 0.001f)
+                {
+                    var segmentToRotate = AllChildSegments[0];
+                    
+                    segmentToRotate.transform.rotation = new Quaternion(0, 0, 0, 0);
+
+                    CurrentMovementState = WormMovementState.FollowPlayer;
+
+                    StartCoroutine(StartLaunch());
+                }
+            }
+        }
+        /// <summary>
+        /// Get a point on the ground for the worm to move to
+        /// </summary>
+        private Vector2 GetGroundPoint()
+        {
+            RaycastHit2D raycastHit2D = Physics2D.Raycast(transform.position, -GeneralFunctions.GetFaceingDirectionY(transform.gameObject), 50f, whatIsGround);
+
+            if (raycastHit2D)
+            {
+                return raycastHit2D.point;
+            }
+            else
+            {
+                return Vector2.zero;
+            }
+        }
+        /// <summary>
+        /// Get the worm launch end point
+        /// </summary>
+        private IEnumerator StartLaunch()
+        {
+            yield return new WaitForSeconds(launchDelay);
+
+            launchEndPoint = transform.TransformDirection(transform.up * endPointMultiplier);
+
+            CurrentMovementState = WormMovementState.IsLaunching;
+        }
+        /// <summary>
+        /// Checks to see if all worm segments are above ground
+        /// </summary>
+        public bool AreAllWormSegmentsUp()
+        {
+            foreach (WormSegment wormSegment in AllChildSegments)
+            {
+                if (wormSegment)
+                {
+                    if (!wormSegment.AboveGround)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        #endregion
 
         #region Health Functions
         /// <summary>
@@ -99,6 +332,10 @@ namespace EnemyCharacter.AI
         /// An array of all child segments
         /// </summary>
         public WormSegment[] AllChildSegments { get; private set; }
+        /// <summary>
+        /// Gets the current movement state of the worm
+        /// </summary>
+        public WormMovementState CurrentMovementState { get; private set; }
         #endregion
     }
 }
--- Assets/Scripts/Enemy Scripts/Worm Scripts/WormSegment.cs
+++ Assets/Scripts/Enemy Scripts/Worm Scripts/WormSegment.cs
@@ -10,11 +10,15 @@ namespace EnemyCharacter.AI
         /// position of this segment in the worm movement segment array
         /// </summary>
         public int Index { get; private set; } = 0;
+        /// <summary>
+        /// Check to see if the segment is above ground
+        /// </summary>
+        public bool AboveGround { get; private set; } = false;
 
         private BoxCollider2D boxCollider2D = null;
         private CapsuleCollider2D capsuleCollider2D = null;
         private SpriteRenderer spriteRenderer = null;
-        private FixedJoint2D fixedJoint2D = null;
+        private LayerMask whatIsGround = new LayerMask();
 
         [System.Serializable]
         public class OnSegmentDeath : UnityEvent<WormSegment> { }
@@ -25,32 +29,87 @@ namespace EnemyCharacter.AI
         /// <summary>
         /// Set all internal worm segment values
         /// </summary>
-        public void SetupWormSegment(int index, float health)
+        public void SetupWormSegment(int index, float health, LayerMask whatIsGround)
         {
-            this.Index = index;
+            Index = index;
 
             MyHealthComponent.SetHealth(health);
+
+            this.whatIsGround = whatIsGround;
 
             boxCollider2D = GetComponent<BoxCollider2D>();
             capsuleCollider2D = GetComponent<CapsuleCollider2D>();
             spriteRenderer = GetComponent<SpriteRenderer>();
-            fixedJoint2D = GetComponent<FixedJoint2D>();
         }
         /// <summary>
         /// Disables collision and hides worm segment sprite's then invokes the segment death event
         /// </summary>
         protected override void OnDeath()
         {
-            boxCollider2D.enabled = false;
-            capsuleCollider2D.enabled = false;
             spriteRenderer.enabled = false;
 
-            if (fixedJoint2D)
-            {
-                fixedJoint2D.enabled = false;
-            }
+            DisableCollision();
 
             SegmentDeath.Invoke(this);
         }
+
+        #region Collision Functions
+        /// <summary>
+        /// Toggle collision between the worm segment layer and ground
+        /// </summary>
+        public void ToggleGroundCollision(bool collide)
+        {
+            var localCollide = !collide;
+
+            var segmentLayer = 22;
+            var groundLayer = 8;
+
+            Physics2D.IgnoreLayerCollision(segmentLayer, groundLayer, localCollide);
+        }
+        /// <summary>
+        /// Enables both collision and fixed joint 2D then makes the rigidbody dynamic
+        /// </summary>
+        public void EnableCollision()
+        {
+            ToggleGroundCollision(true);
+
+            boxCollider2D.enabled = true;
+            capsuleCollider2D.enabled = true;
+
+            MyRigidBody2D.constraints = RigidbodyConstraints2D.None;
+        }
+        /// <summary>
+        /// Disables both collision and fixed joint 2D then freeze rotation and position
+        /// </summary>
+        public void DisableCollision()
+        {
+            ToggleGroundCollision(false);
+
+            boxCollider2D.enabled = false;
+            capsuleCollider2D.enabled = false;
+
+            MyRigidBody2D.constraints = RigidbodyConstraints2D.FreezeAll;
+        }
+        /// <summary>
+        /// Check to see if this segment is overlapping any ground
+        /// </summary>
+        public bool IsOverlappingGround()
+        {
+            Collider2D collider2D = Physics2D.OverlapCapsule(transform.position, capsuleCollider2D.size, capsuleCollider2D.direction, GeneralFunctions.GetObjectEulerAngle(gameObject), whatIsGround);
+
+            if (collider2D)
+            {
+                AboveGround = false;
+
+                return true;
+            }
+            else
+            {
+                AboveGround = true;
+
+                return false;
+            }
+        }
+        #endregion
     }
 }
